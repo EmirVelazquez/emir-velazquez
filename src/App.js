05be95b88151ec1bbfef9a17c909189585e6e08b
EmirVelazquez <emirleovelazquez@gmail.com>
2020-02-23 21:03:44 -0600

    added google fonts link, added custom style sheet. Testing typeface pairings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome User, My name is Emir Velazquez...</h1>
-        <p>React Portfolio</p>
+        <h1 id="userGreet">Welcome User, My name is Emir Velazquez...</h1>
+        <p id="secondaryText">React Portfolio New</p>
       </header>
     </div>
   );
