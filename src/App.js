0397f12831c5875dec6532df011e31aa78b31f9e
EmirVelazquez <emirleovelazquez@gmail.com>
2020-03-09 23:34:30 -0500

    grammar fix

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
       console.log("If you are reading this, let's get in touch. I am looking for opportunities.");
     } else {
       console.log("You are a curious one, that's great.");
-      console.log("Contact me and let's get it your idea done.");
+      console.log("Contact me and let's get your idea or project done.");
       this.setState({ userNameIs: currentUser });
     }
   }
